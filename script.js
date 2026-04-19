037ef70a162e8ddd7dd362091c90da9fa5578f68
Naufaliaaa <naufalzul47@gmail.com>
2026-04-19 20:50:21 +0700

    I LOVE JULE

--- script.js
+++ script.js
@@ -301,4 +301,4 @@ window.addEventListener('scroll', () => {
 
 console.log('Website portofolio berhasil dimuat!');
 
-//Ini file JS untuk Fungsi dan animasi gelooh.
+//Ini file JS untuk Fungsi dan animasi.
