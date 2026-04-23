ca2947106c300f326d24df698c712da0cb47e272
Naufaliaaa <naufalzul47@gmail.com>
2026-04-23 21:50:23 +0700

    Yu ah

--- script.js
+++ script.js
@@ -301,4 +301,4 @@ window.addEventListener('scroll', () => {
 
 console.log('Website portofolio berhasil dimuat!');
 
-//Ini file JS untuk Fungsi dan animasi.
+//ini file JS untuk Fungsi dan animasii.
